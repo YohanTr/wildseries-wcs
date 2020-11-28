e00bef3e15cd418ec150fd48b6a380ec3dc732f5
Yohan Tahir <yohantahir@gmail.com>
2020-11-28 21:01:56 +0100

    show list of series in template program

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,50 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/programs", name="program_")
+ */
+class ProgramController extends AbstractController
+{
+    /**
+     * Show all rows from Program’s entity
+     *
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+        return $this->render(
+            '/program/index.html.twig',
+            ['programs' => $programs]
+        );
+    }
+    /**
+     * @Route("/{id}", requirements={"id"="^[0-9]*[1-9][0-9]*$"}, methods={"GET"}, name="show")
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('/program/show.html.twig', [
+            'program' => $program,
+        ]);
+    }
+}
